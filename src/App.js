1a91e0ab5667096061c00cf33d196ee0d7592461
Proboskis <nikolasurla22@gmail.com>
2022-11-03 14:35:32 +0100

    1. Initial commit with the initial setup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import ReactMarkdown from "react-markdown";
+import {PrismAsyncLight as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {coyWithoutShadows} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function App() {
+    const [input, setInput] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea autoFocus name="textarea" id="textArea" value={input} onChange={
+          (e) => setInput(e.target.value)
+      }>{console.info(input)}</textarea>
+        <ReactMarkdown className="markdown" children={input}
+            components={{
+            code({className, children}) {
+            let match = /language-(\w+)/.exec(className || '')
+            return match ? (
+            <SyntaxHighlighter
+                className="codebox"
+                showLineNumbers={true}
+                wrapLongLines={true}
+                children={String(children).replace(/\n$/, '')}
+                style={coyWithoutShadows}
+                language={match[1]}
+            />
+            ) : (
+            <code className={className}>
+                {children}
+            </code>
+            )
+        }
+        }}/>
     </div>
   );
 }
